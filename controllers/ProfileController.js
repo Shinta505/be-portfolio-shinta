1e0ab43e3e3ecd33dce22912a91c737c7d8426a6
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2026-09-07 18:51:14 +0700

    Fix import statement formatting in ProfileController

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,4 +1,4 @@
-import ProfileModel from "../models/ProfileModel.js";
+import ProfileModel from "../models/ProfileModel.js"; 
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
